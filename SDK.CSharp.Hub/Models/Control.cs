a4d2c2c248ce65cb6b85e1258c41c170fe0bf5c2
Luca <luca@perugia.ovh>
2024-04-24 01:27:18 +0200

    Version 0.0.16

--- SDK.CSharp.Hub/Models/Control.cs
+++ SDK.CSharp.Hub/Models/Control.cs
@@ -11,4 +11,5 @@ public class Control
     public required byte Intensity { get; set; }
     [Range(300, 30000)]
     public required uint Duration { get; set; }
+    public bool Exclusive { get; set; } = false;
 }
